ec7cd64d39be1c861955370819513fb52c1fe0cb
Tayri <tayrifernandez@gmail.com>
2022-10-21 13:16:18 +0100

    animacion menu

--- PracticaInterfaz/Assets/Script/MenuAnimation.cs
+++ PracticaInterfaz/Assets/Script/MenuAnimation.cs
@@ -10,12 +10,36 @@ public class MenuAnimation : MonoBehaviour
     private void OnEnable()
     {
         LeanTween.moveLocalY(logo, -353f, 0f);
-        LeanTween.moveLocalY(buttonGroup, -495f, 0f);
+        //LeanTween.scale(buttonGroup, Vector3.zero, 0f);
+        LeanTween.scale(buttonNewGame, Vector3.zero, 0f);
+        LeanTween.scale(buttonLoadGame, Vector3.zero, 0f);
+        LeanTween.scale(buttonOptions, Vector3.zero, 0f);
+        LeanTween.scale(buttonExit, Vector3.zero, 0f);
+        //LeanTween.moveLocalY(buttonGroup, -495f, 0f);
 
-        LeanTween.moveLocalY(logo, 221.0483f, 1.5f).setEaseSpring();
-
-        
+        LeanTween.moveLocalY(logo, 209.33f, 1.5f).setEaseSpring().setOnComplete(Botones);
         //LeanTween.moveLocalY(buttonGroup, -48.5417f, 1.5f).setEaseSpring();
+
+        void Latido()
+        {
+            LeanTween.scale(logo, Vector3.one, 1f).setEaseSpring().setLoopPingPong();
+        }
+
+        void Botones()
+        {            
+            //LeanTween.scale(buttonGroup, Vector3.one, 0f);
+            LeanTween.scale(buttonNewGame, Vector3.one, 1.5f).setEaseSpring().setOnComplete( () =>
+            {
+                LeanTween.scale(buttonLoadGame, Vector3.one, 1.5f).setEaseSpring().setOnComplete(() =>
+                {
+                    LeanTween.scale(buttonOptions, Vector3.one, 1.5f).setEaseSpring().setOnComplete(() =>
+                    {
+                        LeanTween.scale(buttonExit, Vector3.one, 1.5f).setEaseSpring().setOnComplete(Latido);
+                    });
+                });
+            });
+
+        }
     }
 
 }
